c72537609a5803704b2f7876996fd8b783877083
Mani Gurusankar <manigurusankar@gmail.com>
2024-07-14 19:16:25 -0500

    Implemented: - custom `useUser` hook that calls `useQuery` to cache user data - custom `useUserAppoinments` hook  that calls `useQuery` to cache user appoinments data - `setQueryData` and `removeQueries`

--- client/src/components/user/hooks/useUserAppointments.ts
+++ client/src/components/user/hooks/useUserAppointments.ts
@@ -3,19 +3,30 @@ import type { Appointment } from "@shared/types";
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 
 import { useLoginData } from "@/auth/AuthContext";
+import { generateUserAppointmentKey } from "@/react-query/key-factories";
+import { useQuery } from "@tanstack/react-query";
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   userId: number,
-//   userToken: string
-// ): Promise<Appointment[] | null> {
-//   const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
-//     headers: getJWTHeader(userToken),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  userId: number,
+  userToken: string
+): Promise<Appointment[] | null> {
+  const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
+    headers: getJWTHeader(userToken),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
-  // TODO replace with React Query
-  return [];
+
+  //get details on userId
+  const { userId, userToken } = useLoginData();
+  const fallback: Appointment[] = [];
+  const { data: userAppointments = fallback } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserAppointmentKey(userId, userToken),
+    queryFn: () => getUserAppointments(userId ,userToken)
+  });
+  
+  return userAppointments;
 }
